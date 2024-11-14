eb4349b53211bab77c562afe16f84a173ad8affb
Hisham <hkherbouch@gmail.com>
2024-11-14 11:08:09 -0500

    Added num_vector_sum function and test cases

--- num_vector_utils.cpp
+++ num_vector_utils.cpp
@@ -13,4 +13,13 @@ string render_num_vector(const vector<int>& vec) {
     s.erase(s.length()-1,1);
     return s;
 }
-
+int num_vector_sum(const vector<int>& vec){
+    int sum = 0;
+    if(vec.size()==0){
+        return sum;
+    }
+    for(int i =0; i<vec.size(); i++){
+        sum += vec[i];
+    }
+    return sum;
+}
--- test_num_vector_utils.cpp
+++ test_num_vector_utils.cpp
@@ -10,12 +10,21 @@ TEST_CASE("Testing render_num_vector") {
     string numstr = render_num_vector(nums);
     string expected = "1 3 7";
     CHECK(numstr == expected);
-    vector<int> nums1 = {2, 4, 6};
+    vector<int> nums1 = {2, 4, 6, 8};
     string numstr1 = render_num_vector(nums1);
-    string expected1 = "2 4 6";
+    string expected1 = "2 4 6 8";
     CHECK(numstr1 == expected1);
     vector<int> nums2 = {1, 3, 5};
     string numstr2 = render_num_vector(nums2);
     string expected2 = "1 3 5";
     CHECK(numstr2 == expected2);
 }
+TEST_CASE("Testing num_vector_sum") {
+    vector<int> nums = {1, 3, 7};
+    CHECK(11 == num_vector_sum(nums));
+    vector<int> nums_2 = {2, 4, 6};
+    CHECK(12 == num_vector_sum(nums_2));
+    vector<int> nums_3 = {1, 3, 5, 7};
+    CHECK(16 == num_vector_sum(nums_3));
+}
+
