c7c5c6166a46fa6de52dfd0c5a64bea450a86f00
Hisham <hkherbouch@gmail.com>
2024-11-20 12:31:30 -0500

    Created files for time and testing file

--- chapter11/time.cpp
+++ chapter11/time.cpp
@@ -1,16 +1,1 @@
-#include <vector>
-#include <string>
-#include <iostream>
-#include <algorithm>
 
-using namespace std;
-
-struct Time {
-    int hour, minute;
-    double second;
-}
-
-void print_time(const Time& time)
-{
-    cout << time.hour << ":" << time.minute  << ":" << time.second << endl;
-}
--- chapter11/time.h
+++ chapter11/time.h
@@ -1,10 +1,1 @@
-#include <vector>
-#include <string>
-#include <iostream>
 
-using namespace std;
-
-struct Time {
-    int hour, minute;
-    double second;
-}
