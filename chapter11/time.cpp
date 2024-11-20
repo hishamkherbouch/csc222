dcd7c4c6216b5e4543ec42da1a628c60e53e086d
Hisham <hkherbouch@gmail.com>
2024-11-20 12:52:44 -0500

    Work done during class 11/20. Not much progress.

--- chapter11/time.cpp
+++ chapter11/time.cpp
@@ -1,1 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "Time.h"
+using namespace std;
+
+Time: :Time(int sec){
+	int min = 0;
+	int hr = 0;
+	this->sec = sec;
+	
+}
+
+Time: :Time(int hr, int min){
+	this -> hr = hr;
+	this -> min = min;
+
+	
+}
+
+Time: :Time(int hr, int min, int sec){
+          this -> hr = hr;
+          this -> min = min;
+          this -> sec = sec;
+}
+void Time : :to_string() const{
+}
 
--- chapter11/time.h
+++ chapter11/time.h
@@ -1,1 +1,11 @@
+#include <algorithm>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+struct Time {
+	int sec, min, hr;
+
+	void to_string() const;
+};
