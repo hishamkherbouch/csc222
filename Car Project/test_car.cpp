8f2333d66118d6e6b069a1402e777baf587c2579
Hisham <hkherbouch@gmail.com>
2024-12-03 12:44:08 -0500

    Test case passes

--- Car Project/test_car.cpp
+++ Car Project/test_car.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 TEST_CASE("Test can create and render MyObject") {
     Car myobj("BMW", "White", "X3", 2025, 0, 100.0 );
-    CHECK(myobj.to_string() == "Brand BMW; Color White; Model X3; Year 2025; Miles 0; Gas tank 100.0;");
+    CHECK(myobj.to_string() == "Brand BMW; Color White; Model X3; Year 2025; Miles 0; Gas tank 100.0");
 }
 
