a73726fd909d909923f7884a57a400380b871e38
Hisham <hkherbouch@gmail.com>
2024-12-11 10:32:09 -0500

    Created fraction files

---
+++ Chapter 12/Fraction.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include <doctest.h>
+#include "Fraction.h"
+using namespace std;
---
+++ Chapter 12/Fraction.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <string>
+#include <doctest.h>
+using namespace std;
