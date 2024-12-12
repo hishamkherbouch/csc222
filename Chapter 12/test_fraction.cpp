c2e339df8d8a736912cce3e9f2ff98d88b8b038a
Hisham <hkherbouch@gmail.com>
2024-12-12 08:43:00 -0500

    Added 5th test case

--- Chapter 12/test_fraction.cpp
+++ Chapter 12/test_fraction.cpp
@@ -34,4 +34,9 @@ TEST_CASE("Test Fractions are stored in lowest terms") {
     Fraction f2(8, 16);
     CHECK(f2.to_string() == "1/2");
 }
-
+TEST_CASE("Test integer Fractions render properly") {
+    Fraction f1(5, 1);
+    CHECK(f1.to_string() == "5");
+    Fraction f2(18, 3);
+    CHECK(f2.to_string() == "6");
+}
