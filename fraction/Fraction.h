6f1a5e831c39cda34676eee3e789f626a5451bcc
Hisham <hkherbouch@gmail.com>
2024-12-13 11:35:05 -0500

    Moved fraction to seperate directory

---
+++ fraction/Fraction.h
@@ -0,0 +1,19 @@
+#include <numeric>
+#include <iostream>
+#include <string>
+#include <doctest.h>
+using namespace std;
+
+struct Fraction{
+    int numerator;
+    int denominator;
+
+    Fraction(int,int);
+    Fraction();
+    Fraction(int);
+    Fraction(string);
+   
+    int gcd(int, int);
+    string to_string() const;
+};
+
