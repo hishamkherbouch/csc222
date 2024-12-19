b4f4ae9e028f084166df268698476a3d85fd5d5c
Hisham <hkherbouch@gmail.com>
2024-12-19 11:54:02 -0500

    Added some stuff

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+struct Deck;
+
+enum Suit {NONE, CLUBS, DIAMONDS, HEARTS, SPADES};
+enum Rank {JOKER, TWO=2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
+
 struct Card
 {
     Suit suit;
