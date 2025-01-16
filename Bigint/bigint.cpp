d298fcf32bb459ae7549383a333b583a2bb360dc
Hisham <hkherbouch@gmail.com>
2025-01-16 12:03:29 -0500

    Added more scaffolding

--- Bigint/bigint.cpp
+++ Bigint/bigint.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
-#include "BigInt.h"
+#include "bigint.h"
 #define to_num(c) (c-'0')
 using namespace std;
 
+BigInt::BigInt()
+{
+    flag = false;
+    digits = "0";
+};
--- Bigint/bigint.h
+++ Bigint/bigint.h
@@ -1,3 +1,14 @@
 #include <string>
 using namespace std;
 
+class BigInt
+{
+    bool flag;
+    string digits;
+
+    public:
+    // constructors
+    BigInt();
+    BigInt(int);
+    BigInt(string);
+};
